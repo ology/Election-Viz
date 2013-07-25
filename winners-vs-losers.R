dc4e386c8298a200fdf44915629bfe095d4b2c7f
Gene Boggs <gene.boggs@gmail.com>
2013-07-25 13:17:54 -0700

    Added comments and whitespace

--- winners-vs-losers.R
+++ winners-vs-losers.R
@@ -1,14 +1,21 @@
+#
+# Visualize the winner vs loser in the U.S. presidential election
+#
+
+# Read in the election data.
 elections <- read.table(
   '1900.txt',
-  header=TRUE,
-  skip=2,
-  na.strings="-"
+  header     = TRUE,
+  skip       = 2,
+  na.strings = '-'
 );
 
+# Convert "comma numbers" into integers.
 elections$TOTAL   <- as.numeric(gsub(',', '', elections$TOTAL));
 elections$Votes   <- as.numeric(gsub(',', '', elections$Votes));
 elections$Votes.1 <- as.numeric(gsub(',', '', elections$Votes.1));
 
+# Render the scatter plot.
 plot(
   elections$Votes, elections$Votes.1, 
   main = '1900 Election', 
@@ -16,5 +23,6 @@ plot(
   ylab = 'WILLIAM J. BRYAN'
 );
 
+# Draw a line with slope=1 as a reference.
 abline(0, 1, lty = 2, col = 'blue');
 
