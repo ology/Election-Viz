0f337bf87f7b88886624dbc1eb609030db51491c
Gene Boggs <gene.boggs@gmail.com>
2013-07-25 15:11:20 -0700

    Enabled multi datafile handling

--- winners-vs-losers.R
+++ winners-vs-losers.R
@@ -9,32 +9,39 @@
 # -- gene+github at ology dot net not com
 #
 
-# Read in the election data.
-election <- read.table(
-  'data/1900.txt',
-  header     = TRUE,
-  skip       = 2,
-  na.strings = '-'
+# Collect the election data files.
+filenames <- list.files(
+    'data',
+    pattern = '^presidential-[0-9]{4}.txt$',
+    full.names = TRUE
 );
 
-# Convert the winner numbers into integers.
-election$Votes   <- as.numeric(gsub(',', '', election$Votes));
-# Convert the loser numbers into integers.
-election$Votes.1 <- as.numeric(gsub(',', '', election$Votes.1));
+for (file in filenames) {
+    election <- read.table(
+        file,
+        header     = TRUE,
+        skip       = 2,
+        na.strings = '-'
+    );
 
-# Render the scatter plot.
-plot(
-  election$Votes, election$Votes.1, 
-  main = '1900 Election', 
-  xlab = 'WILLIAM McKINLEY', 
-  ylab = 'WILLIAM J. BRYAN'
-);
+    # Convert the winner numbers into integers.
+    election$Votes   <- as.numeric(gsub(',', '', election$Votes));
+    # Convert the loser numbers into integers.
+    election$Votes.1 <- as.numeric(gsub(',', '', election$Votes.1));
 
-# Draw a slope=1 line as reference.
-abline(
-  a   = 0,
-  b   = 1,
-  lty = 2,
-  col = 'blue'
-);
+    # Render the scatter plot.
+    plot(
+      election$Votes, election$Votes.1, 
+      main = file, 
+      xlab = 'WINNER', 
+      ylab = 'LOSER'
+    );
 
+    # Draw a slope=1 line as reference.
+    abline(
+      a   = 0,
+      b   = 1,
+      lty = 2,
+      col = 'blue'
+    );
+}
